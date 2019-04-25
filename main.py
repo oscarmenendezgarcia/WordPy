d6429cece33fa03ff75b405ebeb29b3ed75b1fe9
Oscar Menéndez García <omenendez.ext@intermarkit.es>
2019-04-25 16:44:08 +0200

    Better error management, refactor code

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ spanish_prepositions = [ "a", "ante", "bajo", "cabo", "con", "contra", "de", "de
 
 def download_audio(url, lang="es"):
         ops = {
-                "format" : "bestaudio/best",
-                "outtmpl" : "",
+                'outtmpl': 'tmp.wav',
+                "format" : "bestaudio/best",                
                 "postprocessors": [{
                 "key": "FFmpegExtractAudio",
-                "preferredcodec": "flac",
+                "preferredcodec": "wav",
                 "preferredquality": "320",
         }],
         }
@@ -20,23 +20,28 @@ def download_audio(url, lang="es"):
 
 def transcribe_audio(path):
         r = sr.Recognizer()
-
-        audioFile = sr.AudioFile("DEBATE ATRESMEDIA 28A - LOS MEJORES MOMENTOS-8qerVyCdlg4.flac")
+        audioFile = sr.AudioFile(path)
         with audioFile as source:
-                audio = r.record(source, duration=90)
-                return r.recognize_google(audio, language="es")
+                try:
+                        audio = r.record(source, duration=120, offset=30)
+                        return r.recognize_google(audio, language="es-ES")
+                except sr.UnknownValueError:
+                        print("Google Speech Recognition could not understand audio")
+                except sr.RequestError as e:
+                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-def create_visualization(text):     
-        wordcloud = WordCloud().generate(text)
+
+
+def create_visualization(text):
+        wordcloud = WordCloud(background_color="white").generate(text)
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis("off")
         plt.show()
 
 
 url = "https://www.youtube.com/watch?v=8qerVyCdlg4"
+path = "tmp.wav"
 
-# download_audio(url)
-
-path = "C:/Users/omenendez.ext/Desktop/WordPy/DEBATE ATRESMEDIA 28A - LOS MEJORES MOMENTOS-8qerVyCdlg4"
+download_audio(url)
 text = transcribe_audio(path)
 create_visualization(text)
